2e94daaf7a08928a2660085792e91d7728f342fe
Mariia <mariiaolden@gmail.com>
2019-01-20 20:42:16 +0100

    navbar

---
+++ cat.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $('.navbar-toggle').on('click.bs.collapse', function(e){
+    if ($(this).data('label-expanded')) {
+      var $toggleLabel = $(this).find('.navbar-toggle-label')
+
+      if ($(this).hasClass('collapsed')) {
+        // Save current label
+        $(this).data('label-collapsed', $toggleLabel.text())
+        // Set new text
+        $toggleLabel.text($(this).data('label-expanded'))
+      }
+      else {
+        $toggleLabel.text($(this).data('label-collapsed'))
+      }
+    }
+  })
+
+});
